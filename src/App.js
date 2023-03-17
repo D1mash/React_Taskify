e6c52f18960c448b5ece10d93945bca7380bb28e
Dimash Omarbayev <omarbaev95@mail.ru>
2023-03-17 22:26:05 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Board from "./pages/Board";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+	return (
+		<div className='App'>
+			<Board />
+		</div>
+	);
+}
+
+export default App;
